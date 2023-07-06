a847ee38d1b73a2cc37de5cee0a83a296202c931
Suraj Gupta <surajrocks1997@gmail.com>
2023-07-06 14:28:11 +0530

    Inorder and Preorder Morris Traversal of Binary Tree

--- src/com/trees/traversals/MorrisTraversal.java
+++ src/com/trees/traversals/MorrisTraversal.java
@@ -15,8 +15,38 @@ public class MorrisTraversal {
         root.right.left = new TreeNode(6);
         root.right.right = new TreeNode(7);
 
-        List<Integer> result = inorder(root);
-        System.out.println(result);
+        List<Integer> inorder = inorder(root);
+        System.out.println(inorder);
+
+        List<Integer> preorder = preorder(root);
+        System.out.println(preorder);
+    }
+
+    private static List<Integer> preorder(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null) return result;
+        TreeNode current = root;
+        while (current != null) {
+            if (current.left == null) {
+                result.add(current.data);
+                current = current.right;
+            } else {
+                TreeNode left = current.left;
+                while (left.right != null && left.right != current)
+                    left = left.right;
+
+                if (left.right == null) {
+                    left.right = current;
+                    result.add(current.data);
+                    current = current.left;
+                } else {
+                    left.right = null;
+                    current = current.right;
+                }
+            }
+        }
+
+        return result;
     }
 
     private static List<Integer> inorder(TreeNode root) {
