ca766d689f8944a0f9896461afb3101ff3d86a12
Suraj Gupta <surajrocks1997@gmail.com>
2023-07-09 18:30:37 +0530

    Inorder Successor and Predecessor in BST

---
+++ src/com/trees/BST/medium/InorderPredecessorInBST.java
@@ -0,0 +1,34 @@
+package com.trees.BST.medium;
+
+import com.trees.TreeNode;
+
+public class InorderPredecessorInBST {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(8);
+        root.left = new TreeNode(5);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(7);
+        root.left.right.left = new TreeNode(6);
+        root.right = new TreeNode(12);
+        root.right.left = new TreeNode(10);
+        root.right.right = new TreeNode(14);
+        root.right.right.left = new TreeNode(13);
+
+        TreeNode p = root.right.left;
+
+        TreeNode result = solve(root, p);
+        System.out.println(result.data);
+    }
+
+    private static TreeNode solve(TreeNode root, TreeNode p) {
+        TreeNode predecessor = null;
+        while (root != null) {
+            if(root.data < p.data){
+                predecessor = root;
+                root = root.right;
+            } else
+                root = root.left;
+        }
+        return predecessor;
+    }
+}
