bd3e4a1c1eaca2d705d54608ee771b423583a8d8
Suraj Gupta <surajrocks1997@gmail.com>
2023-07-21 18:56:30 +0530

    Is Bipartite - Graph

---
+++ src/com/graph/medium/IsBipartite.java
@@ -0,0 +1,45 @@
+package com.graph.medium;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class IsBipartite {
+    public static void main(String[] args) {
+        int[][] graph = {
+                {1, 2, 3},
+                {0, 2},
+                {0, 1, 3},
+                {0, 2}
+        };
+
+        boolean result = solve(graph);
+        System.out.println(result);
+    }
+
+    private static boolean solve(int[][] graph) {
+        int V = graph.length;
+
+        int[] color = new int[V];
+        Arrays.fill(color, -1);
+
+        for (int i = 0; i < V; i++) {
+            if (color[i] == -1) {
+                if (!dfs(i, 0, color, graph)) return false;
+            }
+        }
+        return true;
+    }
+
+    private static boolean dfs(int node, int col, int[] color, int[][] graph) {
+        color[node] = col;
+
+        for (int it : graph[node]) {
+            if (color[it] == -1) {
+                if (!dfs(it, 1 - col, color, graph)) return false;
+            } else if (color[it] == col) return false;
+        }
+
+        return true;
+    }
+}
