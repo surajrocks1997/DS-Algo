5bcbcc0a799a84a77663a39ae451a9c624957627
Suraj Gupta <surajrocks1997@gmail.com>
2023-07-23 23:09:34 +0530

    Eventual Safe States in Graph

---
+++ src/com/graph/medium/EventualSafeStates.java
@@ -0,0 +1,58 @@
+package com.graph.medium;
+
+import java.util.*;
+
+public class EventualSafeStates {
+    public static void main(String[] args) {
+        int[][] graph = {
+                {1, 2},
+                {2, 3},
+                {5},
+                {0},
+                {5},
+                {},
+                {}
+        };
+
+        List<Integer> result = solve(graph);
+        System.out.println(result);
+    }
+
+    private static List<Integer> solve(int[][] graph) {
+        int V = graph.length;
+        List<List<Integer>> adjRev = new ArrayList<>();
+        for (int i = 0; i < V; i++) {
+            adjRev.add(new ArrayList<>());
+        }
+
+        int[] indegree = new int[V];
+        for (int i = 0; i < V; i++) {
+            for (int it : graph[i]) {
+                adjRev.get(it).add(i);
+                indegree[i]++;
+            }
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < V; i++) {
+            if (indegree[i] == 0)
+                queue.add(i);
+        }
+
+        List<Integer> safeNodes = new ArrayList<>();
+
+        while (!queue.isEmpty()) {
+            int node = queue.poll();
+            safeNodes.add(node);
+
+            for (int it : adjRev.get(node)) {
+                indegree[it]--;
+                if (indegree[it] == 0)
+                    queue.add(it);
+            }
+        }
+
+        Collections.sort(safeNodes);
+        return safeNodes;
+    }
+}
