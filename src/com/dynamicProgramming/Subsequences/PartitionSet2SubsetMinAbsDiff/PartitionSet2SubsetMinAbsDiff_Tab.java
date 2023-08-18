8e64cbf8333c173b2cd06f246c0463f6fc66d558
Suraj Gupta <surajrocks1997@gmail.com>
2023-08-18 18:16:51 +0530

    Partitioning to 2 Subset of Equal length and minimize Absoulte Difference

--- src/com/dynamicProgramming/Subsequences/PartitionSet2SubsetMinAbsDiff/PartitionSet2SubsetMinAbsDiff_Tab.java
+++ src/com/dynamicProgramming/Subsequences/PartitionSet2SubsetMinAbsDiff/PartitionSet2SubsetMinAbsDiff_Tab.java
@@ -2,7 +2,7 @@ package com.dynamicProgramming.Subsequences.PartitionSet2SubsetMinAbsDiff;
 
 import java.util.Arrays;
 
-public class PartitionSet2SubsetMinAbsDiff_Rec {
+public class PartitionSet2SubsetMinAbsDiff_Tab {
     public static void main(String[] args) {
         int[] nums = {2, -1, 0, 4, -2, -9};
 
@@ -26,24 +26,3 @@ public class PartitionSet2SubsetMinAbsDiff_Rec {
         return Math.min(pick, notPick);
     }
 }
-
-
-// another working solution but TLE issue
-
-// class Solution {
-//     public int minimumDifference(int[] nums) {
-//         int totalSum = Arrays.stream(nums).sum();
-//         return solve(nums.length - 1, nums.length / 2, 0, Integer.MAX_VALUE, totalSum, nums);
-//     }
-
-//     private static int solve(int index, int count, int sum, int mini, int totalSum, int[] nums) {
-//         if (count == 0)
-//             return Math.min(mini, Math.abs(sum - (totalSum - sum)));
-//         if (index == 0 && count != 0)
-//             return mini;
-
-//         int pick = solve(index - 1, count - 1, sum + nums[index], mini, totalSum, nums);
-//         int notPick = solve(index - 1, count, sum, mini, totalSum, nums);
-//         return Math.min(pick, notPick);
-//     }
-// }
