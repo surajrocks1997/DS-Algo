b584d7c9f822a15d639739acd509eb2ede610571
Suraj Gupta <surajrocks1997@gmail.com>
2023-08-29 22:04:17 +0530

    Longest Substring without repeating characters

--- src/com/SlidingWindow_2Pointer/LongestSubstringWithoutRepeatingChar.java
+++ src/com/SlidingWindow_2Pointer/LongestSubstringWithoutRepeatingChar.java
@@ -5,24 +5,29 @@ import java.util.Map;
 
 public class LongestSubstringWithoutRepeatingChar {
     public static void main(String[] args) {
-        String s = "abba";
+        String s = "tmmzuxt";
         int result = solve(s);
         System.out.println(result);
     }
 
     private static int solve(String s) {
-        int left = 0, right = 0;
-        int window = 0;
+        int windowStart = 0;
+        int windowSize = 0;
         Map<Character, Integer> map = new HashMap<>();
-        while (right < s.length()) {
-            if (map.containsKey(s.charAt(right)))
-                left = Math.max(map.get(s.charAt(right)) + 1, left);
 
-            map.put(s.charAt(right), right);
-            window = Math.max(right-left+1, window);
-            right++;
+        for (int windowEnd = 0; windowEnd < s.length(); windowEnd++) {
+            if (map.containsKey(s.charAt(windowEnd))) {
+                while (s.charAt(windowStart) != s.charAt(windowEnd)) {
+                    map.remove(s.charAt(windowStart++));
+                }
+                map.remove(s.charAt(windowStart++));
+            }
+
+            map.put(s.charAt(windowEnd), 1);
+
+
+            windowSize = Math.max(windowSize, windowEnd - windowStart + 1);
         }
-
-        return window;
+        return windowSize;
     }
 }
