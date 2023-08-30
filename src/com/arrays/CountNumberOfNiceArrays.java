c2b7ce3db82e5183392945446b07bc5929fdcc9d
Suraj Gupta <surajrocks1997@gmail.com>
2023-08-30 16:56:17 +0530

    Count number of nice arrays

---
+++ src/com/arrays/CountNumberOfNiceArrays.java
@@ -0,0 +1,41 @@
+package com.arrays;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CountNumberOfNiceArrays {
+    public static void main(String[] args) {
+        int[] nums = {2,2,2,1,2,2,1,2,2,2};
+        int k = 2;
+
+        int result = solve(nums, k);
+        System.out.println(result);
+    }
+
+    private static int solve(int[] nums, int k) {
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            nums[i] = nums[i] % 2 == 0 ? 0 : 1;
+        }
+
+        if (n == 1 && nums[0] != k) return 0;
+
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        int prefixSum = 0;
+        int count = 0;
+
+        int index = 0;
+        while (index < n) {
+            prefixSum += nums[index];
+            int rem = prefixSum - k;
+
+            count += map.getOrDefault(rem, 0);
+
+            map.put(prefixSum, map.getOrDefault(prefixSum, 0) + 1);
+            index++;
+        }
+
+        return count;
+    }
+}
