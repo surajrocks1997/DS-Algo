6377bef8a8ba86d0648a940dbd1a1a0f3c6407c9
Suraj Gupta <surajrocks1997@gmail.com>
2023-08-31 14:38:37 +0530

    Longest Subarray with Given Sum K

---
+++ src/com/arrays/LongestSubArrayWithSumK.java
@@ -0,0 +1,28 @@
+package com.arrays;
+
+public class LongestSubArrayWithSumK {
+    public static void main(String[] args) {
+        int[] a = {1, 2, 3, 1, 1, 1, 1};
+        int k = 3;
+
+        int result = solve(a, k);
+        System.out.println(result);
+    }
+
+    private static int solve(int[] a, int k) {
+        int windowStart = 0;
+        int max = 0;
+        long sum = 0;
+
+        for (int windowEnd = 0; windowEnd < a.length; windowEnd++) {
+            sum += a[windowEnd];
+
+            if (sum > k) {
+                while (sum > k) sum -= a[windowStart++];
+            }
+            if (sum == k)
+                max = Math.max(max, windowEnd - windowStart + 1);
+        }
+        return max;
+    }
+}
