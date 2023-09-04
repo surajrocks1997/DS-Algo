59c5e4e5eb291e433d9abe29b83ef0712fc5b779
Suraj Gupta <surajrocks1997@gmail.com>
2023-09-04 22:36:41 +0530

    Palindrome Partitioning

--- src/com/recursion/hard/PalindromePartitioning.java
+++ src/com/recursion/hard/PalindromePartitioning.java
@@ -3,7 +3,7 @@ package com.recursion.hard;
 import java.util.ArrayList;
 import java.util.List;
 
-public class PalindromePartioning {
+public class PalindromePartitioning {
     public static void main(String[] args) {
         String str = "aabb";
         List<List<String>> result = solve(str);
@@ -12,29 +12,30 @@ public class PalindromePartioning {
 
     private static List<List<String>> solve(String str) {
         List<List<String>> result = new ArrayList<>();
-        List<String> current = new ArrayList<>();
-        solve(0, str, current, result);
+        List<String> curr = new ArrayList<>();
+        solve(0, str, curr, result);
         return result;
     }
 
-    private static void solve(int index, String str, List<String> current, List<List<String>> result) {
+    private static void solve(int index, String str, List<String> curr, List<List<String>> result) {
         if (index == str.length()) {
-            result.add(new ArrayList<>(current));
+            result.add(new ArrayList<>(curr));
             return;
         }
 
         for (int i = index; i < str.length(); i++) {
             if (isPalindrome(index, i, str)) {
-                current.add(str.substring(index, i + 1));
-                solve(i + 1, str, current, result);
-                current.remove(current.size() - 1);
+                curr.add(str.substring(index, i+1));
+                solve(i + 1, str, curr, result);
+                curr.remove(curr.size() - 1);
             }
         }
     }
 
     private static boolean isPalindrome(int left, int right, String str) {
         while (left < right) {
-            if (str.charAt(left++) != str.charAt(right--)) return false;
+            if (str.charAt(left++) != str.charAt(right--))
+                return false;
         }
         return true;
     }
