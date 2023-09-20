9bb22ff74854ef2af5c6f1f1be5de469dc1c7f8b
Suraj Gupta <surajrocks1997@gmail.com>
2023-09-20 14:30:09 +0530

    Next Permutation code updated

--- src/com/arrays/nextPermutation.java
+++ src/com/arrays/nextPermutation.java
@@ -1,5 +1,7 @@
 package com.arrays;
 
+import java.util.Arrays;
+
 public class nextPermutation {
     public static void main(String[] args) {
         int[] arr = {2, 1, 5, 4, 3, 0, 0};
@@ -8,36 +10,29 @@ public class nextPermutation {
         for (int ele : arr) System.out.print(ele + " ");
     }
 
-    private static void solve(int[] arr) {
-        if (arr.length == 1) return;
-
-        int i = -1;
-        for (i = arr.length - 2; i >= 0; i--) {
-            if (arr[i] < arr[i + 1]) break;
-        }
-        if (i == -1) {
-            reverse(arr, 0, arr.length - 1);
-            return;
-        }
-
-        for (int ind = arr.length - 1; ind >= i; ind--) {
-            if (arr[ind] > arr[i]) {
-                swap(arr, i, ind);
+    private static void solve(int[] arr) { // logic is same as used in String->medium->NextGreaterNumberWithGivenSetOfNumber
+        int i = 0;
+        int n = arr.length;
+        for (i = n - 1; i > 0; i--) {
+            if (arr[i] > arr[i - 1])
                 break;
+        }
+
+        int min = i;
+
+        if (i == 0) {
+            Arrays.sort(arr);
+        } else {
+            for (int index = i; index < n; index++) {
+                if (arr[index] > arr[i - 1] && arr[min] > arr[index])
+                    min = index;
             }
-        }
-        reverse(arr, i+1, arr.length-1);
-    }
 
-    private static void reverse(int[] arr, int left, int right) {
-        while (left < right) {
-            swap(arr, left++, right--);
-        }
-    }
+            int temp = arr[i - 1];
+            arr[i - 1] = arr[min];
+            arr[min] = temp;
 
-    private static void swap(int[] arr, int left, int right) {
-        int temp = arr[left];
-        arr[left] = arr[right];
-        arr[right] = temp;
+            Arrays.sort(arr, i, n);
+        }
     }
 }
