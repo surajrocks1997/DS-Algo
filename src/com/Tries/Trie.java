8f52e7523e51f4e0f5947bdceaa19dca8ff33852
Suraj Gupta <surajrocks1997@gmail.com>
2023-09-26 18:40:03 +0530

    code structure change

--- src/com/Tries/Trie.java
+++ src/com/Tries/Trie.java
@@ -1,32 +1,5 @@
 package com.Tries;
 
-class Node {
-    Node[] links = new Node[26];
-    boolean flag = false;
-
-    public Node() {}
-
-    public boolean containsKey(char ch) {
-        return (links[ch - 'a'] != null);
-    }
-
-    public Node get(char ch) {
-        return links[ch - 'a'];
-    }
-
-    public void put(char ch, Node node) {
-        links[ch - 'a'] = node;
-    }
-
-    public void setEnd() {
-        flag = true;
-    }
-
-    public boolean isEnd() {
-        return flag;
-    }
-}
-
 class Trie {
     private final Node root;
 
@@ -70,5 +43,33 @@ class Trie {
             node = node.get(prefix.charAt(i));
         }
         return true;
+    }
+}
+
+class Node {
+    Node[] links = new Node[26];
+    boolean flag = false;
+
+    public Node() {
+    }
+
+    public boolean containsKey(char ch) {
+        return (links[ch - 'a'] != null);
+    }
+
+    public Node get(char ch) {
+        return links[ch - 'a'];
+    }
+
+    public void put(char ch, Node node) {
+        links[ch - 'a'] = node;
+    }
+
+    public void setEnd() {
+        flag = true;
+    }
+
+    public boolean isEnd() {
+        return flag;
     }
 }
