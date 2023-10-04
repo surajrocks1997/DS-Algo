aa7ae5790afda0823b78a7fe0efaaab6a6174ba3
Suraj Gupta <surajrocks1997@gmail.com>
2023-10-04 22:31:19 +0530

    Min Cost to cut a stick - solution using tabulation

---
+++ src/com/dynamicProgramming/MCM/MinCostToCutStick/MinCostToCutStick_Tab.java
@@ -0,0 +1,43 @@
+package com.dynamicProgramming.MCM.MinCostToCutStick;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class MinCostToCutStick_Tab {
+    public static void main(String[] args) {
+        int n = 7;
+        int[] cuts = {3, 5, 1, 4};
+
+        int result = solve(cuts, n);
+        System.out.println(result);
+    }
+
+    private static int solve(int[] cuts, int n) {
+        List<Integer> myCuts = new ArrayList<>();
+        Arrays.stream(cuts).forEach(myCuts::add);
+        myCuts.add(0);
+        myCuts.add(n);
+        Collections.sort(myCuts);
+        int[][] dp = new int[cuts.length + 2][cuts.length + 2];
+
+//        base cases not required as it is equal to 0
+
+        for (int i = cuts.length; i >= 1; i--) {
+            for (int j = 1; j <= cuts.length; j++) {
+                if (i > j) continue;
+                int min = Integer.MAX_VALUE;
+                for (int index = i; index <= j; index++) {
+                    int cost = myCuts.get(j + 1) - myCuts.get(i - 1) +
+                            dp[i][index - 1] + dp[index + 1][j];
+
+                    min = Math.min(min, cost);
+                }
+                dp[i][j] = min;
+            }
+        }
+
+        return dp[1][cuts.length];
+    }
+}
