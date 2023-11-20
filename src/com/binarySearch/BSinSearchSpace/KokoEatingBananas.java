e02f866c923bce0bfd6c67454a27ca6449d97331
Suraj Gupta <surajrocks1997@gmail.com>
2023-11-20 22:44:50 +0530

    Koko eating bananas - all test case passing

--- src/com/binarySearch/BSinSearchSpace/KokoEatingBananas.java
+++ src/com/binarySearch/BSinSearchSpace/KokoEatingBananas.java
@@ -1,10 +1,6 @@
 package com.binarySearch.BSinSearchSpace;
 
-//        Koko loves to eat bananas. There are n piles of bananas, the ith pile has piles[i] bananas. The guards have gone and will come back in h hours.
-//        Koko can decide her bananas-per-hour eating speed of k. Each hour, she chooses some pile of bananas and eats k bananas from that pile.
-//        If the pile has less than k bananas, she eats all of them instead and will not eat any more bananas during this hour.
-//        Koko likes to eat slowly but still wants to finish eating all the bananas before the guards return.
-//        Return the minimum integer k such that she can eat all the bananas within h hours.
+// https://leetcode.com/problems/koko-eating-bananas/
 
 //        Example 1:
 //        Input: piles = [3,6,7,11], h = 8
@@ -20,36 +16,32 @@ package com.binarySearch.BSinSearchSpace;
 
 public class KokoEatingBananas {
     public static void main(String[] args) {
-        int[] piles = {805306368, 805306368, 805306368};
+        int[] piles = {805306368, 805306368, 805306368}; // 2415919104 > 1000000000
         int h = 1000000000;
         int result = solve(piles, h);
         System.out.println(result);
     }
 
     private static int solve(int[] piles, int h) {
-        int max = Integer.MIN_VALUE;
-        for (int ele : piles)
-            max = Math.max(ele, max);
+        int lowSpeed = 1;
+        int highSpeed = (int) 1e12;
 
-        int low = 1;
-        int high = max;
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
 
-            if (calculate(mid, piles) <= h) high = mid - 1;
-            else low = mid + 1;
-        }
+        while(lowSpeed <= highSpeed){
+            int midSpeed = lowSpeed + (highSpeed-lowSpeed)/2;
 
-        return low;
-    }
+            int timeTakenToEatAllBanana = 0;
+            for(int i = 0; i < piles.length; i++){
+                timeTakenToEatAllBanana += (int) Math.ceil((double) piles[i] / (double) midSpeed);
+            }
 
-    private static int calculate(int mid, int[] piles) {
-        int sum = 0;
-        for (int ele : piles) {
-            sum += ele / mid;
-            if (ele % mid != 0) sum++;
+            if(timeTakenToEatAllBanana <= h)
+                highSpeed = midSpeed - 1;
+            else
+                lowSpeed = midSpeed + 1;
 
         }
-        return sum;
+
+        return lowSpeed;
     }
 }
