1d209eff5fa7c4856a7bb14722569694cd0baf6a
Suraj Gupta <surajrocks1997@gmail.com>
2024-02-04 15:09:02 +0530

    better code

--- src/com/Strings/Hard/MinWindowSubstring.java
+++ src/com/Strings/Hard/MinWindowSubstring.java
@@ -15,96 +15,49 @@ public class MinWindowSubstring {
     }
 
     private static String solve(String s, String t) {
-        if (s.length() == 0 || s.length() < t.length()) return "";
+        int tLen = t.length();
+        int sLen = s.length();
+        if (sLen < tLen) return "";
 
-        Map<Character, Integer> map = new HashMap<>();
-        for (int i = 0; i < t.length(); i++) {
-            map.put(t.charAt(i), map.getOrDefault(t.charAt(i), 0) + 1);
+        Map<Character, Integer> tMap = new HashMap<>();
+
+        for (int i = 0; i < tLen; i++) {
+            tMap.put(t.charAt(i), tMap.getOrDefault(t.charAt(i), 0) + 1);
         }
+        int required = tMap.size();
+        int have = 0;
 
-        int count = 0;
-        int start = 0;
+        Map<Character, Integer> sMap = new HashMap<>();
+        int i = 0;
+        int j = 0;
         int minLength = Integer.MAX_VALUE;
-        int minLeft = 0;
+        int leftPointer = -1;
 
-        for (int i = 0; i < s.length(); i++) {
-            if (map.containsKey(s.charAt(i))) {
-                map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) - 1);
-
-                if (map.get(s.charAt(i)) >= 0) {
-                    count++;
-                }
+        while (j < sLen) {
+            if (tMap.containsKey(s.charAt(j))) {
+                sMap.put(s.charAt(j), sMap.getOrDefault(s.charAt(j), 0) + 1);
+                if (sMap.get(s.charAt(j)).equals(tMap.get(s.charAt(j))))
+                    have++;
             }
 
 
-            while (count == t.length()) {
-                if (minLength > i - start + 1) {
-                    minLength = i - start + 1;
-                    minLeft = start;
+            while (have == required) {
+                if (j - i + 1 < minLength) {
+                    minLength = j - i + 1;
+                    leftPointer = i;
                 }
 
-                if (map.containsKey(s.charAt(start))) {
-                    map.put(s.charAt(start), map.get(s.charAt(start)) + 1);
-                    if (map.get(s.charAt(start)) > 0) count--;
+                if (tMap.containsKey(s.charAt(i))) {
+                    sMap.put(s.charAt(i), sMap.get(s.charAt(i)) - 1);
+                    if (sMap.get(s.charAt(i)) < tMap.get(s.charAt(i)))
+                        have--;
                 }
-                start++;
+                i++;
             }
+            j++;
         }
 
-        if (minLength > s.length()) return "";
-        return s.substring(minLeft, minLeft + minLength);
+        if (minLength > sLen) return "";
+        return s.substring(leftPointer, leftPointer + minLength);
     }
 }
-
-// another valid solution
-
-//class Solution {
-//    public String minWindow(String s, String t) {
-//        int tLen = t.length();
-//        int sLen = s.length();
-//        if(sLen < tLen) return "";
-//
-//        Map<Character, Integer> tMap = new HashMap<>();
-//
-//        for (int i = 0; i < tLen; i++) {
-//            tMap.put(t.charAt(i), tMap.getOrDefault(t.charAt(i), 0) + 1);
-//        }
-//        int required = tMap.size();
-//        int have = 0;
-//
-//        Map<Character, Integer> sMap = new HashMap<>();
-//        int i = 0;
-//        int j = 0;
-//        int minLength = Integer.MAX_VALUE;
-//        String resultString = "";
-//        boolean updated = false;
-//
-//        while (j < sLen) {
-//            if (tMap.containsKey(s.charAt(j))) {
-//                sMap.put(s.charAt(j), sMap.getOrDefault(s.charAt(j), 0) + 1);
-//                if (sMap.get(s.charAt(j)) == (tMap.get(s.charAt(j))))
-//                    have++;
-//            }
-//
-//
-//            while (have == required) {
-//                if(j-i + 1 < minLength){
-//                    minLength = j-i + 1;
-//                    resultString = s.substring(i, j + 1);
-//                    updated = true;
-//                }
-//
-//                if (tMap.containsKey(s.charAt(i))) {
-//                    sMap.put(s.charAt(i), sMap.get(s.charAt(i)) - 1);
-//                    if (sMap.get(s.charAt(i)) < tMap.get(s.charAt(i)))
-//                        have--;
-//                }
-//                i++;
-//            }
-//            j++;
-//        }
-//
-//        if(updated) return resultString;
-//        else return "";
-//    }
-//}
