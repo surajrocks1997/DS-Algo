fb7c6926f2744867937c62b404a10b4aa3ceb9f2
Suraj Gupta <surajrocks1997@gmail.com>
2024-02-04 14:51:13 +0530

    neet code approach

--- src/com/Strings/Hard/MinWindowSubstring.java
+++ src/com/Strings/Hard/MinWindowSubstring.java
@@ -3,6 +3,8 @@ package com.Strings.Hard;
 import java.util.HashMap;
 import java.util.Map;
 
+//https://leetcode.com/problems/minimum-window-substring/description
+
 public class MinWindowSubstring {
     public static void main(String[] args) {
         String s = "ADOBECODEBANC";
@@ -53,3 +55,56 @@ public class MinWindowSubstring {
         return s.substring(minLeft, minLeft + minLength);
     }
 }
+
+// another valid solution
+
+//class Solution {
+//    public String minWindow(String s, String t) {
+//        int tLen = t.length();
+//        int sLen = s.length();
+//        if(sLen < tLen) return "";
+//
+//        Map<Character, Integer> tMap = new HashMap<>();
+//
+//        for (int i = 0; i < tLen; i++) {
+//            tMap.put(t.charAt(i), tMap.getOrDefault(t.charAt(i), 0) + 1);
+//        }
+//        int required = tMap.size();
+//        int have = 0;
+//
+//        Map<Character, Integer> sMap = new HashMap<>();
+//        int i = 0;
+//        int j = 0;
+//        int minLength = Integer.MAX_VALUE;
+//        String resultString = "";
+//        boolean updated = false;
+//
+//        while (j < sLen) {
+//            if (tMap.containsKey(s.charAt(j))) {
+//                sMap.put(s.charAt(j), sMap.getOrDefault(s.charAt(j), 0) + 1);
+//                if (sMap.get(s.charAt(j)) == (tMap.get(s.charAt(j))))
+//                    have++;
+//            }
+//
+//
+//            while (have == required) {
+//                if(j-i + 1 < minLength){
+//                    minLength = j-i + 1;
+//                    resultString = s.substring(i, j + 1);
+//                    updated = true;
+//                }
+//
+//                if (tMap.containsKey(s.charAt(i))) {
+//                    sMap.put(s.charAt(i), sMap.get(s.charAt(i)) - 1);
+//                    if (sMap.get(s.charAt(i)) < tMap.get(s.charAt(i)))
+//                        have--;
+//                }
+//                i++;
+//            }
+//            j++;
+//        }
+//
+//        if(updated) return resultString;
+//        else return "";
+//    }
+//}
