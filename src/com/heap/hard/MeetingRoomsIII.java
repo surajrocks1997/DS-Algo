cf20a2c9d1e71584b9628601e3f6bb48f0f5f5fa
Suraj Gupta <surajrocks1997@gmail.com>
2024-02-18 17:36:04 +0530

    Meeting Rooms III

--- src/com/heap/hard/MeetingRoomsIII.java
+++ src/com/heap/hard/MeetingRoomsIII.java
@@ -24,8 +24,12 @@ public class MeetingRoomsIII {
     }
 
     private static int solve(int[][] meetings, int n) {
+        //sorting meeting[][] based on start time, if start time is same then smaller endTime should come first
         Arrays.sort(meetings, (a, b) -> a[0] != b[0] ? a[0] - b[0] : a[1] - b[1]);
+
         PriorityQueue<RoomDetail> availQ = new PriorityQueue<>((a, b) -> Integer.compare(a.roomNo, b.roomNo));
+
+        // smaller end time will come first, if end time is same then smaller roomNo will come first
         PriorityQueue<BusyQ> busyQ = new PriorityQueue<>((a, b) ->
                 a.endTime != b.endTime ? Long.compare(a.endTime, b.endTime) : a.roomDetail.roomNo - b.roomDetail.roomNo);
 
@@ -35,11 +39,18 @@ public class MeetingRoomsIII {
             long currentStart = meetingTime[0];
             long currentEnd = (long) meetingTime[1];
 
+            // checking if current meeting's start time is <= smallest end time of busyQ
+            // if true then emptying the busyROom and adding to freeRoom
             while (!busyQ.isEmpty() && busyQ.peek().endTime <= currentStart) {
                 BusyQ pollBusy = busyQ.poll();
                 availQ.add(pollBusy.roomDetail);
             }
 
+            // if no rooms available, then find the earliest endTime of meeting is busyQ
+            // update the start time of delayed meeting to earliest endTime of meeting is busyQ
+            // poll fromBusyQ and add new meeting to busyQ
+
+            //else, since we have rooms available, directly add a meeting to busyQ and poll from availQ
             if (availQ.isEmpty()) {
                 Long duration = currentEnd - currentStart;
                 Long newStart = busyQ.peek().endTime;
@@ -58,7 +69,7 @@ public class MeetingRoomsIII {
             availQ.add(pollBusy.roomDetail);
         }
 
-
+        // sorting based on highest count, if count is same, then room with smaller number comes first
         return availQ.stream().sorted((a, b) -> a.count != b.count ? b.count - a.count : a.roomNo - b.roomNo).toList().get(0).roomNo;
 
     }
