f0222918a27f1c1daa9f2d386655c34139e0d630
Suraj Gupta <surajrocks1997@gmail.com>
2024-09-23 22:59:15 +0530

    added both recursive and iterative solution

--- src/com/trees/LexicographicalNumbers.java
+++ src/com/trees/LexicographicalNumbers.java
@@ -14,9 +14,11 @@ public class LexicographicalNumbers {
 
     private static List<Integer> solve(int n) {
         List<Integer> list = new ArrayList<>();
-        for (int i = 1; i <= 9; i++) {
-            dfs(i, n, list);
-        }
+//        for (int i = 1; i <= 9; i++) {
+//            dfs(i, n, list);
+//        }
+
+        iterativeSolution(1, n, list);
 
         return list;
     }
@@ -28,6 +30,21 @@ public class LexicographicalNumbers {
         for (int idx = 0; idx <= 9; idx++) {
             if (i * 10 + idx > n) break;
             dfs(i * 10 + idx, n, list);
+        }
+    }
+
+    private static void iterativeSolution(int i, int n, List<Integer> list) {
+        int curr = i;
+        while (list.size() < n) {
+            list.add(curr);
+
+            if (curr * 10 <= n)
+                curr = curr * 10;
+            else {
+                while (curr == n || curr % 10 == 9)
+                    curr = curr / 10;
+                curr += 1;
+            }
         }
     }
 }
