5cc6b5a796c09582ec57796c263eeaf8070dc3c8
Suraj Gupta <surajrocks1997@gmail.com>
2026-06-21 19:36:25 +0530

    set matrix 0 optimized solution

--- src/com/arrays/SetMatrixZero.java
+++ src/com/arrays/SetMatrixZero.java
@@ -1,5 +1,6 @@
 package com.arrays;
 
+//https://leetcode.com/problems/set-matrix-zeroes/
 public class SetMatrixZero {
     public static void main(String[] args) {
         int[][] arr = {
@@ -7,8 +8,14 @@ public class SetMatrixZero {
                 {3, 4, 5, 2},
                 {1, 3, 1, 5}
         };
+
+//        int[][] arr = {
+//                {1, 0, 1},
+//                {1, 0, 1},
+//                {0, 1, 1}
+//        };
         printMatrix(arr);
-        solve(arr);
+        solve1(arr);
         System.out.println("Result:");
         printMatrix(arr);
     }
@@ -20,6 +27,56 @@ public class SetMatrixZero {
             }
             System.out.println();
         }
+        System.out.println("\n");
+    }
+
+    // O(1) space solution
+    private static void solve1(int[][] matrix) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int rowZero = 1;
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[0][j] = 0;
+
+                    if (i > 0) matrix[i][0] = 0;
+                    else rowZero = 0;
+                }
+            }
+        }
+
+        printMatrix(matrix);
+
+        // row and col >= 1
+        for (int i = 1; i < rows; i++) {
+            for (int j = 1; j < cols; j++) {
+                if (matrix[0][j] == 0 || matrix[i][0] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        printMatrix(matrix);
+
+        // first row 0th column
+        if (matrix[0][0] == 0) {
+            for (int i = 0; i < rows; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+
+        printMatrix(matrix);
+
+        // first column 0th row
+        if (rowZero == 0) {
+            for (int j = 0; j < cols; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+
+        printMatrix(matrix);
     }
 
     private static void solve(int[][] matrix) {
