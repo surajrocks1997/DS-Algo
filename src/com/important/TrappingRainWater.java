e2e734b4ad7646947f64d45359e42e64076c62ad
Suraj Gupta <surajrocks1997@gmail.com>
2026-07-20 11:33:36 +0530

    trapping rain water

--- src/com/important/TrappingRainWater.java
+++ src/com/important/TrappingRainWater.java
@@ -1,5 +1,7 @@
 package com.important;
 
+import java.util.Arrays;
+
 public class TrappingRainWater {
     public static void main(String[] args) {
         int[] height = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
@@ -13,17 +15,21 @@ public class TrappingRainWater {
         int[] suffix = new int[n];
         prefix[0] = arr[0];
         for (int i = 1; i < n; i++) {
-            prefix[i] = Math.max(prefix[i - 1], arr[i]);
+            prefix[i] = Math.max(prefix[i - 1], arr[i - 1]);
         }
+        Arrays.stream(prefix).forEach(ele -> System.out.print(ele + " "));
+        System.out.println();
 
         suffix[n - 1] = arr[n - 1];
         for (int i = n - 2; i >= 0; i--) {
-            suffix[i] = Math.max(suffix[i + 1], arr[i]);
+            suffix[i] = Math.max(suffix[i + 1], arr[i + 1]);
         }
+        Arrays.stream(suffix).forEach(ele -> System.out.print(ele + " "));
 
         int waterTrapped = 0;
         for (int i = 0; i < n; i++) {
-            waterTrapped += Math.min(prefix[i], suffix[i]) - arr[i];
+            int res = Math.min(prefix[i], suffix[i]) - arr[i];
+            waterTrapped += Math.max(res, 0);
         }
         return waterTrapped;
     }
